ad89b3e39059ce5195384a1a90e0edc5621292eb
Rethinakumar, Prabu <PRABU_RETHINAKUMAR@homedepot.com>
2017-10-27 10:55:18 -0400

    First commit

--- FlaskR.py
+++ FlaskR.py
@@ -7,7 +7,7 @@ port = int(os.environ.get("PORT", 3000))
 
 @app.route('/')
 def run_job():
-	print("Hello World")
+	return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
